4741e47c1acb18e57e8905d4b7205ae23d99c8a0
Bhuman Soni <bhuman@yourdiagrams.com>
2015-06-29 15:32:37 +1000

    added the extractUrl method

--- Html2CanvasProxy.java
+++ Html2CanvasProxy.java
@@ -12,18 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Html2CanvasProxy extends HttpServlet{
     private static final long serialVersionUID = -6856074403887401174L;
+    /*
+    if the url is something like,
+    "url=http%3A%2F%2Fdomain.com%2Fimages%2Fimage.gif";
+    so extract the required contents of the url to fetch the image
+    */
+    public void extractUrl(String url){
+        return url.replaceAll("url=", "").replaceAll("%3A", ":").replaceAll("%2F", "/");
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Process request with content.
-        /*
-        the url can be something like this below
-        String queryUrl = "http://www.popgunchaos.com/wp-content/uploads/2011/04/Tick.jpg";
-        */
         String queryUrl = extractUrl(request.getQueryString());
-        
+
         File file = new File(queryUrl);
-        String filename = file.getName().toString(); 
-        
+        String filename = file.getName().toString();
+
         URL url = new URL(queryUrl);
         URLConnection connection = url.openConnection();
         InputStream stream = connection.getInputStream();
